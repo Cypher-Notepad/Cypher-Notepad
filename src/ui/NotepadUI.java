8dd78014958aeb983bab1662429ee2c235e05ee9
LeeDongGeon <secmatth1996@gmail.com>
2020-05-20 14:52:55 +0900

    update urls

--- src/ui/NotepadUI.java
+++ src/ui/NotepadUI.java
@@ -719,7 +719,7 @@ public class NotepadUI extends JFrame implements UI {
 			if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
 				try {
 					Desktop.getDesktop()
-					.browse(new URI("https://leedonggeon1996.github.io/Cypher-Notepad-Web/"));
+					.browse(new URI("https://Cypher-Notepad.github.io"));
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				} catch (URISyntaxException e1) {
