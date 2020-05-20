985570bf96defa8cced309e6d9d24be730c5b23b
LeeDongGeon <secmatth1996@gmail.com>
2020-05-20 14:22:18 +0900

    update Kprinter

--- src/ui/custom/KPrinter.java
+++ src/ui/custom/KPrinter.java
@@ -5,25 +5,68 @@ import java.awt.event.ActionListener;
 import java.awt.print.*;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
 import javax.swing.JTextArea;
+import javax.swing.plaf.basic.BasicTextUI;
+import javax.swing.text.BoxView;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Element;
+import javax.swing.text.StyleContext;
+import javax.swing.text.View;
 
 
 public class KPrinter implements ActionListener, Printable {
 
+	private JTextArea textAreaToPrint;
 	private JTextArea textArea;
+	protected PrintView m_printView;
+	protected DefaultStyledDocument m_doc;
+    StyleContext m_context;
 
 	public KPrinter(JTextArea textArea) {
-		this.textArea = textArea;
+		this.textAreaToPrint = textArea;
 	}
-
-	public int print(Graphics gx, PageFormat pf, int page) throws PrinterException {
-		if (page > 0) {
-			return NO_SUCH_PAGE;
-		}
-		Graphics2D g = (Graphics2D) gx;
-		g.translate(pf.getImageableX(), pf.getImageableY());
-		g.drawString(textArea.getText(), 100, 100);
-		return PAGE_EXISTS;
+	
+	private void createDummyTextArea() {
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		m_context = new StyleContext();
+		m_doc = new DefaultStyledDocument(m_context);
+		textArea.setDocument(m_doc);
+	}
+	
+	public int print(Graphics pg, PageFormat pageFormat, int pageIndex) throws PrinterException {
+		
+		createDummyTextArea();
+        textArea.setText(textAreaToPrint.getText());
+		
+		pg.translate((int)pageFormat.getImageableX(),
+		        (int)pageFormat.getImageableY());
+		        int wPage = (int)pageFormat.getImageableWidth();
+		        int hPage = (int)pageFormat.getImageableHeight();
+		        pg.setClip(0, 0, wPage, hPage);
+		         
+		        // Only do this once per print
+		        if (m_printView == null) {
+		            BasicTextUI btui = (BasicTextUI)textArea.getUI();
+		            View root = btui.getRootView( textArea );
+		            m_printView = new PrintView(
+		            m_doc.getDefaultRootElement(),
+		            root, wPage, hPage);
+		        }
+		         
+		        boolean bContinue = m_printView.paintPage(pg,
+		        hPage, pageIndex);
+		        System.gc();
+		         
+		        if (bContinue)
+		            return PAGE_EXISTS;
+		        else {
+		            m_printView = null;
+		            return NO_SUCH_PAGE;
+		        }
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -46,5 +89,44 @@ public class KPrinter implements ActionListener, Printable {
 		}
 		
 	}
+	
+    private class PrintView extends BoxView {
+        protected int m_firstOnPage = 0;
+        protected int m_lastOnPage = 0;
+        protected int m_pageIndex = 0;
+         
+        public PrintView(Element elem, View root, int w, int h) {
+            super(elem, Y_AXIS);
+            setParent(root);
+            setSize(w, h);
+            layout(w, h);
+        }
+         
+        public boolean paintPage(Graphics g, int hPage,
+        int pageIndex) {
+            if (pageIndex > m_pageIndex) {
+                m_firstOnPage = m_lastOnPage + 1;
+                if (m_firstOnPage >= getViewCount())
+                    return false;
+                m_pageIndex = pageIndex;
+            }
+            int yMin = getOffset(Y_AXIS, m_firstOnPage);
+            int yMax = yMin + hPage;
+            Rectangle rc = new Rectangle();
+             
+            for (int k = m_firstOnPage; k < getViewCount(); k++) {
+                rc.x = getOffset(X_AXIS, k);
+                rc.y = getOffset(Y_AXIS, k);
+                rc.width = getSpan(X_AXIS, k);
+                rc.height = getSpan(Y_AXIS, k);
+                if (rc.y+rc.height > yMax)
+                    break;
+                m_lastOnPage = k;
+                rc.y -= yMin;
+                paintChild(g, rc, k);
+            }
+            return true;
+        }
+    }
 
 }
